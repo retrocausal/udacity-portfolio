943754aee9facae578740e14d3d65d0fc9122b6d
Nitin Prabhakar <pmitwithnitin@gmail.com>
2017-12-16 23:10:31 +0530

    - Rescale images   - reduce sizes   - force jpeg conversion  - rewrite css per rubric   - two classes inside of polymer used format classname element     - changed to just classname  - add footer content  - rewrite portfolio.js to only insert <article-card> polymer element on load

--- gulpfile.js
+++ gulpfile.js
@@ -2,52 +2,78 @@ const g = require('gulp');
 const $ = require('gulp-load-plugins')();
 let i_destination = './bundle/responsive-assets/';
 
-const i_optimize = function () {
-  return g.src(['./assets/**/*.{png,jpg,jpeg}'])
+const i_optimize = function ()
+{
+  return g.src(['./assets/**/*.{png,jpg,jpeg,giff,webp}'])
     .pipe($.plumber())
     .pipe($.assetCache.filter('./assets/asset-cache'))
-    .pipe($.responsive({
-      '**/*': [{
-        width: 480,
-        rename: {
-          suffix: '-small'
-        }
-      }, {
-        width: 640,
-        rename: {
-          suffix: '-medium'
-        }
-      }, {
-        width: 960,
-        rename: {
-          suffix: '-large'
-        }
-      }, {
-        width: 1280,
-        rename: {
-          suffix: '-x-large'
-        }
-      }, {
-        width: 1600,
-        rename: {
-          suffix: '-xx-large'
-        }
+    .pipe($.responsive(
+    {
+      '**/*': [
+        {
+          width: 480,
+          format: "jpeg",
+          rename:
+          {
+            suffix: '-small'
+          }
+      },
+        {
+          width: 640,
+          format: "jpeg",
+          rename:
+          {
+            suffix: '-medium'
+          }
+      },
+        {
+          width: 960,
+          format: "jpeg",
+          rename:
+          {
+            suffix: '-large'
+          }
+      },
+        {
+          width: 1280,
+          format: "jpeg",
+          rename:
+          {
+            suffix: '-x-large'
+          }
+      },
+        {
+          width: 1600,
+          format: "jpeg",
+          rename:
+          {
+            suffix: '-xx-large'
+          }
       }]
-    }, {
+    },
+    {
       withMetadata: false,
       errorOnEnlargement: false,
       progressive: true,
       withoutEnlargement: false,
-      crop: 'entropy'
+      crop: 'entropy',
+      quality: 40,
+      compressionLevel: 9,
+      min: true,
+      flatten: true,
+      force: false
     }))
     .pipe($.imagemin([
-      $.imagemin.jpegtran({
+      $.imagemin.jpegtran(
+      {
         progressive: true
       }),
-      $.imagemin.optipng({
+      $.imagemin.optipng(
+      {
         optimizationLevel: 9
       })
-    ], {
+    ],
+    {
       verbose: true
     }))
     .pipe($.plumber.stop())
@@ -55,10 +81,12 @@ const i_optimize = function () {
     .pipe($.assetCache.cache());
 };
 
-const css_optimize = function () {
+const css_optimize = function ()
+{
   return g.src('./styles/**/*.css')
     .pipe($.concatCss('app.css'))
-    .pipe($.autoprefixer({
+    .pipe($.autoprefixer(
+    {
       browsers: ['last 2 versions']
     }))
     .pipe(g.dest('./bundle/style/'))
@@ -67,9 +95,11 @@ const css_optimize = function () {
     .pipe(g.dest('./bundle/style/minified/'));
 };
 
-g.task('default', function () {
+g.task('default', function ()
+{
   return i_optimize();
 });
-g.task('csso', function () {
+g.task('csso', function ()
+{
   return css_optimize();
 });
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,89 +1,132 @@
-const projects = [{
-  title: "Arcade Game",
-  description: `A clone of classic arcade games such as Asteroids, Frogger and more, developed using front end technologies such as
-  HTML Canvas, Css and Javascript.
-  The Idea is to hone the latest approaches in object oriented JS, Syntax changes that led to ES5/6 and to incorporate the entire game on an HTML 5 Canvas element.
-  Challenges include abstracting collition detection, dynamic enemy instantiation and more`,
-  coverImg: {
-    src: "./bundle/responsive-assets/pngs/arcade-game",
-    format: "png",
-    caption: "Picture of a classic arcade game"
-  },
-  featured: true,
-  meta: {
-    skills: ['ES5/6', 'Css', 'HTML'],
-    beginDate: "January '18'",
-    endDate: "February'18",
-    liveAt: "",
-    github: "https://www.github.com/udacity"
-  }
-}, {
-  title: "The 50 cent Portfolio",
-  description: `A Professional Portfolio generator, that gathers various skills used, project timelines, and product details, to generate a portfolio such as the one this webpage essays.
-  Initially conceptualized as "Just another porfolio", The new challenge is to monetize these efforts that went into building this webpage.
-  The product once hosted, should gather all required information, and spew out an archive of the bleeding edge code used, so the user / consumer, can
-  without any technical ability, be able to download and self host their portfolios`,
-  coverImg: {
-    src: "./bundle/responsive-assets/pngs/portfolio",
-    format: "png",
-    caption: ""
-  },
-  featured: false,
-  meta: {
-    skills: ['ES5/6', 'Css', 'HTML', 'PolymerJS', 'Webcomponents'],
-    beginDate: "December '17'",
-    endDate: "February'18",
-    liveAt: "",
-    github: "https://www.github.com/udacity"
-  }
-}, {
-  title: "Neighborhood Map",
-  description: `A Web Application, that channels the power of google maps and the algorithms behind it, to display a map.
-  The Map, has to have features to enable a user, to pin their favorite locations in a specific area, preferrably local to their neighborhood.
-  The map should also on hover, display information about any location, the user may choose to pin `,
-  coverImg: {
-    src: "./bundle/responsive-assets/pngs/map",
-    format: "png",
-    caption: ""
-  },
-  featured: false,
-  meta: {
-    skills: ['ES5/6', 'Css', 'HTML', 'polymerJS'],
-    beginDate: "February '18'",
-    endDate: "March'18",
-    liveAt: "",
-    github: "https://www.github.com/udacity"
-  }
-}, {
-  title: "Web Optimization",
-  description: `A Product that simplifies various optimizations, that enable a user to quicken load times.
-  Challenges include deploying service-workers,caching, and critical path styling`,
-  coverImg: {
-    src: "./bundle/responsive-assets/jpegs/webopt",
-    format: "jpg",
-    caption: ""
-  },
-  featured: false,
-  meta: {
-    skills: ['ES5/6'],
-    beginDate: "March '18'",
-    endDate: "March'18",
-    liveAt: "",
-    github: "https://www.github.com/udacity"
-  }
-}];
-
-document.addEventListener('DOMContentLoaded', (event) => {
-  let articleGrid = document.querySelector('.article-grid');
-  window.addEventListener('WebComponentsReady', () => {
-    for (const project of projects) {
-      const article = document.createElement('Article');
-      // let html = `<article-card unresolved="unresolved"></article-card>`;
-      // article.innerHTML = html;
-      article.classList.add("project-card");
+const projects = [
+  {
+    title: "Arcade Game",
+    description: `A clone of classic arcade games such as Asteroids, Frogger and more, developed using front end technologies such as
+HTML Canvas, Css and Javascript.
+The Idea is to hone the latest approaches in object oriented JS, Syntax changes that led to ES5/6 and to incorporate the entire game on an HTML 5 Canvas element.
+Challenges include abstracting collition detection, dynamic enemy instantiation and more`,
+    coverImg:
+    {
+      src: "./bundle/responsive-assets/pngs/arcade-game",
+      format: "png",
+      caption: "Picture of a classic arcade game"
+    },
+    featured: true,
+    meta:
+    {
+      skills: [
+'ES5/6', 'Css', 'HTML'
+],
+      beginDate: "January '18'",
+      endDate: "February'18",
+      liveAt: "",
+      github: "https://www.github.com/udacity"
+    },
+    container: "portfolioproject1"
+},
+  {
+    title: "The 50 cent Portfolio",
+    description: `A Professional Portfolio generator, that gathers various skills used, project timelines, and product details, to generate a portfolio such as the one this webpage essays.
+Initially conceptualized as "Just another porfolio", The new challenge is to monetize these efforts that went into building this webpage.
+The product once hosted, should gather all required information, and spew out an archive of the bleeding edge code used, so the user / consumer, can
+without any technical ability, be able to download and self host their portfolios`,
+    coverImg:
+    {
+      src: "./bundle/responsive-assets/pngs/portfolio",
+      format: "png",
+      caption: ""
+    },
+    featured: false,
+    meta:
+    {
+      skills: [
+'ES5/6', 'Css', 'HTML', 'PolymerJS', 'Webcomponents'
+],
+      beginDate: "December '17'",
+      endDate: "February'18",
+      liveAt: "",
+      github: "https://www.github.com/udacity"
+    },
+    container: "portfolioproject2"
+},
+  {
+    title: "Neighborhood Map",
+    description: `A Web Application, that channels the power of google maps and the algorithms behind it, to display a map.
+The Map, has to have features to enable a user, to pin their favorite locations in a specific area, preferrably local to their neighborhood.
+The map should also on hover, display information about any location, the user may choose to pin `,
+    coverImg:
+    {
+      src: "./bundle/responsive-assets/pngs/map",
+      format: "png",
+      caption: ""
+    },
+    featured: false,
+    meta:
+    {
+      skills: [
+'ES5/6', 'Css', 'HTML', 'polymerJS'
+],
+      beginDate: "February '18'",
+      endDate: "March'18",
+      liveAt: "",
+      github: "https://www.github.com/udacity"
+    },
+    container: "portfolioproject3"
+},
+  {
+    title: "Web Optimization",
+    container: "portfolioproject4",
+    description: `A Product that simplifies various optimizations, that enable a user to quicken load times.
+Challenges include deploying service-workers,caching, and critical path styling`,
+    coverImg:
+    {
+      src: "./bundle/responsive-assets/jpegs/webopt",
+      format: "jpg",
+      caption: ""
+    },
+    featured: false,
+    meta:
+    {
+      skills: ['ES5/6'],
+      beginDate: "March '18'",
+      endDate: "March'18",
+      liveAt: "",
+      github: "https://www.github.com/udacity"
+    }
+}
+];
+document.addEventListener('DOMContentLoaded', () =>
+{
+  document.querySelector(".date")
+    .innerHTML = new Date()
+    .getFullYear();
+  window.addEventListener('WebComponentsReady', () =>
+  {
+    for (const project of projects)
+    {
       let aCard = new articleCard(project);
-      article.appendChild(aCard);
-      articleGrid.appendChild(article);
+      document.querySelector("#" + project.container)
+        .appendChild(aCard);
     }
   });
 });
+
+
+
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   let articleGrid = document.querySelector('.article-grid');
+//   window.addEventListener('WebComponentsReady', () => {
+//     for (const project of projects) {
+//       const article = document.createElement('Article');
+//        let html = `<article-card unresolved="unresolved"></article-card>`;
+//        article.innerHTML = html;
+//       article.classList.add("project-card");
+//       let aCard = new articleCard(project);
+//       article.appendChild(aCard);
+//       articleGrid.appendChild(article);
+//     }
+//   });
+// });
+
+//let [portfolioProject1, portfolioProject2, portfolioProject3, portfolioProject4] = [...projects];
